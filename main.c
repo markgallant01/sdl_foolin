b05445ba494c128ab3f6043e7a8713208036a8a2
markgallant01 <markgallant01@gmail.com>
2025-01-10 18:54:48 -0500

    add images and logic to display them based on keypress

--- main.c
+++ main.c
@@ -4,6 +4,14 @@
 const int SCREEN_WIDTH = 640;
 const int SCREEN_HEIGHT = 480;
 
+enum KeyPressSurfaces {
+    KEY_PRESS_SURFACE_UP,
+    KEY_PRESS_SURFACE_DOWN,
+    KEY_PRESS_SURFACE_LEFT,
+    KEY_PRESS_SURFACE_RIGHT,
+    KEY_PRESS_SURFACE_TOTAL
+};
+
 struct App {
     SDL_Window *window;
     SDL_Surface *windowSurface;
@@ -12,13 +20,13 @@ struct App {
 bool game_initializeGame(struct App *app);
 SDL_Surface *game_loadMedia(void);
 void game_close(struct App *app);
+bool game_loadImages(SDL_Surface *arr[]);
 
 int main(void)
 {
     struct App app;
-    if (!game_initializeGame(&app)) {
+    if (!game_initializeGame(&app))
         return EXIT_FAILURE;
-    }
 
     // load media
     SDL_Surface *background = game_loadMedia();
@@ -33,8 +41,47 @@ int main(void)
     // update the surface
     SDL_UpdateWindowSurface(app.window);
 
-    // hack for window to stay up
-    SDL_Event e; bool quit = false; while( quit == false ){ while( SDL_PollEvent( &e ) ){ if( e.type == SDL_QUIT ) quit = true; } }
+    // store different background images
+    SDL_Surface *images[KEY_PRESS_SURFACE_TOTAL];
+    if (!game_loadImages(images))
+        return EXIT_FAILURE;
+
+    // quit flag
+    bool quit = false;
+
+    // event handler
+    SDL_Event e;
+
+    while (!quit) {
+        // handle events on queue
+        while (SDL_PollEvent(&e) != 0) {
+            if (e.type == SDL_QUIT)
+                quit = true;
+
+            SDL_Surface *newImage = NULL;
+            int keysym = e.key.keysym.sym;
+            switch (keysym) {
+                case SDLK_ESCAPE:
+                    quit = true;
+                    break;
+                case SDLK_UP:
+                    newImage = images[KEY_PRESS_SURFACE_UP];
+                    break;
+                case SDLK_DOWN:
+                    newImage = images[KEY_PRESS_SURFACE_DOWN];
+                    break;
+                case SDLK_LEFT:
+                    newImage = images[KEY_PRESS_SURFACE_RIGHT];
+                    break;
+                case SDLK_RIGHT:
+                    newImage = images[KEY_PRESS_SURFACE_LEFT];
+                    break;
+            }
+
+            SDL_BlitSurface(newImage, NULL, app.windowSurface, NULL);
+            SDL_UpdateWindowSurface(app.window);
+        }
+    }
 
     game_close(&app);
     return 0;
@@ -83,6 +130,36 @@ SDL_Surface *game_loadMedia(void)
     }
 
     return s;
+}
+
+// initialize array of surface pointers
+bool game_loadImages(SDL_Surface *arr[])
+{
+    arr[KEY_PRESS_SURFACE_UP] = SDL_LoadBMP("dots.bmp");
+    if (arr[KEY_PRESS_SURFACE_UP] == NULL) {
+        fprintf(stderr, "Error loading image: %s\n", SDL_GetError());
+        return false;
+    }
+
+    arr[KEY_PRESS_SURFACE_DOWN] = SDL_LoadBMP("snail.bmp");
+    if (arr[KEY_PRESS_SURFACE_DOWN] == NULL) {
+        fprintf(stderr, "Error loading image: %s\n", SDL_GetError());
+        return false;
+    }
+
+    arr[KEY_PRESS_SURFACE_LEFT] = SDL_LoadBMP("colors.bmp");
+    if (arr[KEY_PRESS_SURFACE_LEFT] == NULL) {
+        fprintf(stderr, "Error loading image: %s\n", SDL_GetError());
+        return false;
+    }
+
+    arr[KEY_PRESS_SURFACE_RIGHT] = SDL_LoadBMP("buck.bmp");
+    if (arr[KEY_PRESS_SURFACE_RIGHT] == NULL) {
+        fprintf(stderr, "Error loading image: %s\n", SDL_GetError());
+        return false;
+    }
+
+    return true;
 }
 
 void game_close(struct App *app)
