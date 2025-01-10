48862659c5922f24209a381ae481d9227713ca59
markgallant01 <markgallant01@gmail.com>
2025-01-10 14:55:21 -0500

    clean up

--- main.c
+++ main.c
@@ -4,47 +4,60 @@
 const int SCREEN_WIDTH = 640;
 const int SCREEN_HEIGHT = 480;
 
+struct App {
+    SDL_Window *window;
+    SDL_Surface *windowSurface;
+};
+
+bool game_initializeGame(struct App *app);
+bool game_loadMedia(void);
+void game_close(void);
+
 int main(void)
 {
-    // render window
-    SDL_Window *window = NULL;
+    struct App app;
+    game_initializeGame(&app);
 
-    // window surface
-    SDL_Surface *screenSurface = NULL;
+    // fill surface white
+    SDL_FillRect(app.windowSurface, NULL,
+            SDL_MapRGB(app.windowSurface->format, 0xFF, 0xFF, 0xFF));
 
-    // initialize SDL
-    if (SDL_Init(SDL_INIT_VIDEO) < 0) {
-        printf("SDL could not initialize! SDL_Error: %s\n",
-                SDL_GetError());
-    } else {
-        // create window
-        window = SDL_CreateWindow("SDL Tutorial",
-                SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED,
-                SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN);
-        if (window == NULL) {
-            printf("Window could not be created! SDL_ERROR: %s\n",
-                    SDL_GetError());
-        } else {
-            // get window surface
-            screenSurface = SDL_GetWindowSurface(window);
+    // update the surface
+    SDL_UpdateWindowSurface(app.window);
 
-            // fill surface white
-            SDL_FillRect(screenSurface, NULL,
-                    SDL_MapRGB(screenSurface->format, 0xFF, 0xFF, 0xFF));
-
-            // update the surface
-            SDL_UpdateWindowSurface(window);
-
-            // hack for window to stay up
-            SDL_Event e; bool quit = false; while( quit == false ){ while( SDL_PollEvent( &e ) ){ if( e.type == SDL_QUIT ) quit = true; } }
-        }
-    }
+    // hack for window to stay up
+    SDL_Event e; bool quit = false; while( quit == false ){ while( SDL_PollEvent( &e ) ){ if( e.type == SDL_QUIT ) quit = true; } }
 
     // destroy window
-    SDL_DestroyWindow(window);
+    SDL_DestroyWindow(app.window);
 
     // quit subsystems
     SDL_Quit();
 
     return 0;
+}
+
+bool game_initializeGame(struct App *app)
+{
+    // initialize SDL
+    if (SDL_Init(SDL_INIT_VIDEO) < 0) {
+        fprintf(stderr, "SDL could not initialize! SDL_Error: %s\n",
+                SDL_GetError());
+        return false;
+    }
+
+    // create window
+    app->window = SDL_CreateWindow("SDL Tutorial",
+            SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED,
+            SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN);
+    if (app->window == NULL) {
+        fprintf(stderr, "Window could not be created! SDL_ERROR: %s\n",
+                SDL_GetError());
+        return false;
+    }
+
+    // get window surface
+    app->windowSurface = SDL_GetWindowSurface(app->window);
+
+    return true;
 }
